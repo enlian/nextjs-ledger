9b1a67be7afe1854b8c224cd2402fcb16e893b6c
enlian <frejaenlian@gmail.com>
2024-12-27 13:56:28 +0900

    add db config

--- app/api/addRecord/route.ts
+++ app/api/addRecord/route.ts
@@ -1,7 +1,27 @@
 import { NextResponse } from "next/server";
+import { query } from "./../../libs/db.js";
 
 export async function POST(request: { json: () => any }) {
-  const data = await request.json();
-  console.log(data);
-  return NextResponse.json({ message: "successed", data: data });
+  const { type, tag, date, money } = await request.json();
+
+  return query(
+    `insert into records (type,tag,date,money)
+  values($1,$2,$3,$4)
+  returning *;
+  `,
+    [type, tag, date, money]
+  )
+    .then((res) => {
+      return NextResponse.json({ status: "success", data: res.rows[0] });
+    })
+    .catch((error) => {
+      console.error("Database error:", error);
+      return NextResponse.json(
+        {
+          status: "error",
+          error: error.message,
+        },
+        { status: 500 }
+      );
+    });
 }

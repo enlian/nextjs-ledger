01aa5ad8e77306d434eae899590ca1077de196cb
enlian <frejaenlian@gmail.com>
2024-12-29 20:21:05 +0900

    获取数据接口

---
+++ app/api/getRecords/route.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import { query } from "./../../libs/db.js";
+
+export async function GET() {
+  return query(`select * from records`)
+    .then((res) => {
+      return NextResponse.json({ status: "success", data: res.rows });
+    })
+    .catch((error) => {
+      console.error("Database error:", error);
+      return NextResponse.json(
+        {
+          status: "error",
+          error: error.message,
+        },
+        { status: 500 }
+      );
+    });
+}

56545fd2e3932c2bbfb8e039040965cafe8467b6
enlian <frejaenlian@gmail.com>
2025-01-05 20:33:49 +0900

    优化逻辑

--- app/details/MonthPicker.tsx
+++ app/details/MonthPicker.tsx
@@ -5,16 +5,20 @@ interface props {
   onClick: () => void;
   currentYear: number;
   currentMonth: number;
+  cost: number;
+  income: number;
 }
-const MonthPicker = ({ onClick, currentYear, currentMonth }: props) => {
+const MonthPicker = ({ onClick, currentYear, currentMonth,cost,income }: props) => {
   return (
     <div className={styles.box}>
       <div className={styles.time} onClick={onClick}>
-        <span>{currentYear}年{currentMonth}月</span>
-        <FaCaretDown size={16}/>
+        <span>
+          {currentYear}年{currentMonth}月
+        </span>
+        <FaCaretDown size={16} />
       </div>
-      <div className={styles.money}>总支出￥500</div>
-      <div className={styles.money}>总收入￥50</div>
+      <div className={styles.money}>总支出￥{cost}</div>
+      <div className={styles.money}>总收入￥{income}</div>
     </div>
   );
 };
--- app/details/page.tsx
+++ app/details/page.tsx
@@ -28,6 +28,8 @@ export default function name() {
   const [successAlert, setSuccessAlertOpen] = useState(false);
   const [failedAlert, setFailedAlertOpen] = useState(false);
   const [loading, setLoading] = useState(true);
+  const [cost, setCost] = useState(0);
+  const [income, setIncome] = useState(0);
   const [listData, setListData] = useState<{ date: string; items: item[] }[]>(
     []
   );
@@ -73,10 +75,16 @@ export default function name() {
           //中间件，做分组管理，快速索引
           const dateMap: { [date: string]: { date: string; items: item[] } } =
             {};
-
+          let cost = 0;
+          let income = 0;
           data.data.forEach((i: item) => {
             const date = moment.unix(Number(i.date)).format("YYYY-MM-DD");
-
+            //计算总收入 总支出
+            if (i.type === "支出") {
+              cost = cost + Number(i.money);
+            } else {
+              income = income + Number(i.money);
+            }
             //如果这个时间分组在缓存里不存在，说明应该新建一个时间分组
             if (!dateMap[date]) {
               // 初始化新分组
@@ -88,9 +96,9 @@ export default function name() {
             // 找到对应日期的分组，并将当前 item 添加到 items 数组中
             dateMap[date].items.push(i);
           });
-
           console.log(newData);
-
+          setCost(cost);
+          setIncome(income);
           setListData(newData);
         } else {
           throw new Error("error");
@@ -150,6 +158,8 @@ export default function name() {
         onClick={openTimePickerModal}
         currentYear={currentYear}
         currentMonth={currentMonth}
+        cost={cost}
+        income={income}
       />
 
       {loading ? (
